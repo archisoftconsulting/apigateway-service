9f43d4b3b373fe987d4bacdb4d72b3a87bc097e5
vagrant <vagrant@vagrant>
2017-05-14 17:00:29 +0000

    /api/userservice and /api/greetingservice

--- app/controllers/ApigatewayController.java
+++ app/controllers/ApigatewayController.java
@@ -7,9 +7,11 @@ import java.util.concurrent.ExecutionException;
 import javax.inject.Inject;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Strings;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import play.Logger;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSResponse;
 import play.mvc.Controller;
@@ -17,13 +19,9 @@ import play.mvc.Result;
 
 public class ApigatewayController extends Controller {
 
-	
-	 
 	@Inject WSClient ws;
 	
-	
-	
-	public Result auth(String requestedLocation) throws InterruptedException, ExecutionException{
+	public Result auth(String service) throws InterruptedException, ExecutionException{
 		
 		/* TODO:
 		 * 1. get user requested location,methods
@@ -38,17 +36,55 @@ public class ApigatewayController extends Controller {
 		 */
 		
 		/*
-		 * apigateway - request: POST http://userservice:8080/api/user HTTP/1.1
+		 * apigateway - initial request: GET http://localhost:9000/api/userservice
+		 * apigateway - Request: GET https://gturnquist-quoters.cfapps.io/api/random
 		 * userservice - process display saved json detail
 		   apigateway - Response 200
 		 * 
 		 */
 		
-		//Hard code the user request for testing
-		requestedLocation = "https://gturnquist-quoters.cfapps.io/api/random";
 		
-		String requestedMethod = "GET";
-		String response ="apigateway response...";
+		
+		String response="";
+		String requestLocation=getRequestLocation();
+		
+		if(Strings.isNullOrEmpty(requestLocation))
+		{
+			response = "Access Denied...";
+		}
+		else
+		{
+			switch (request().method()) {
+			case "GET":
+				CompletionStage<JsonNode> jsonPromise = ws.url(requestLocation).get()
+		        .thenApply(WSResponse::asJson);
+				response += jsonPromise.toCompletableFuture().get().toString();
+				
+				break;
+			case "POST":
+				break;
+			case "DELETE":
+				break;
+			case "PATCH":
+				break;
+			case "PUT":
+				break;
+			default:
+				response +="unknown endpoint method";
+				break;
+			}
+		
+		}
+		
+		Logger.info("Response " + response);
+		return ok(response);
+	}
+	
+	private String getRequestLocation()
+	{
+		String requestPath=request().path().toLowerCase().trim();
+		String requestMethod=request().method().toLowerCase().trim();
+		String requestLocation="";
 		
 		List<? extends Config> configList = ConfigFactory.load("endpoints.conf").getConfigList("api.gateway.endpoints");
 		int configListSize = configList.size();
@@ -57,61 +93,21 @@ public class ApigatewayController extends Controller {
 		int i=0;
 		while(!found && i<configListSize)
 		{
-			if (requestedLocation.toLowerCase().trim().equals(configList.get(i).getString("location").toLowerCase().trim()))
+			boolean pathExist = false;
+			boolean methodExist = false;
+			pathExist = requestPath.equals(configList.get(i).getString("path").toLowerCase().trim());
+			methodExist = requestMethod.equals(configList.get(i).getString("method").toLowerCase().trim());
+			
+			if (pathExist && methodExist)
 			{
+				requestLocation=configList.get(i).getString("location").trim();
 				found=true;
 			}
 			i++;
 		}
+		Logger.info("Request:" + requestMethod + " " + requestLocation);
 		
-		if(found){
-			
-			response += serviceDispatch(requestedMethod, requestedLocation) + "===" + parseRequest();
-		}
-		else
-		{
-			response += "access denied";
-		}
-		
-		return ok(response);
+		return requestLocation;
 	}
-	
-	private String serviceDispatch(String requestedMethod,String requestedLocation) throws InterruptedException, ExecutionException{
-		String response="";
-		
-		switch (requestedMethod) {
-		case "GET":
-			CompletionStage<JsonNode> jsonPromise = ws.url(requestedLocation).get()
-	        .thenApply(WSResponse::asJson);
-			response += jsonPromise.toCompletableFuture().get().toString();
-			break;
-		case "POST":
-			break;
-		case "DELETE":
-			break;
-		case "PATCH":
-			break;
-		case "PUT":
-			break;
-		default:
-			response +="unknown endpoint method";
-			break;
-		}
-		
-		return response;
-	}
-	
-	private String parseRequest(){
-		 // set to "/login" -- The URI path without query parameters.
-	    String path = request().path(); 
 
-	    // set to "/login?param=test" -- The full URI.
-	    String uri = request().uri(); 
-
-	    // set to "example.com:9000" -- The host name from the request, with port (if specified by the client).
-	    String host = request().host(); 
-	    
-	    return "path=" + path +";full url=" + uri +";host=" + host;
-	    
-	}
 }
